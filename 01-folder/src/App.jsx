23b559a6eaad7f8ce39bdd5568a4db326160185e
Shourya Gupta <shouyagupta19@gmail.com>
2026-03-30 14:49:39 +0530

    01-folder done

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <div id="dad">
+        <h1 id="child1">Hey Shourya</h1>
+        <h2 id="child">Hey Shourya React</h2>
+      </div>
+      <div id="chacha">
+        <h3>Hello Gulo kese ho!!!</h3>
+        
+      </div>
+    </>
+  )
+}
+
+export default App;
